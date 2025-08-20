307b2d3c168ef1c3eba162444e6eec42af08c11a
Osama <osamadoage0@gmail.com>
2025-08-21 00:14:55 +0300

    bugfix release.

--- src/controllers/Invoice/get.controller.ts
+++ src/controllers/Invoice/get.controller.ts
@@ -4,8 +4,14 @@ import { InvoiceDocument } from "../../types/models/invoice.types.js";
 import { responseMessages } from "../../translation/response.ar.js";
 import { InvoiceResponseDto, toInvoiceResponseDto } from "../../types/dtos/invoice.dto.js";
 import { PharmacistDocument } from "../../types/models/pharmacist.types.js";
+import Section from "../../models/section.model.js";
+import { FeeDocument } from "../../types/models/fee.types.js";
 
-const getInvoice = async (req: Request, res: TypedResponse<InvoiceResponseDto>, next: NextFunction) => {
+const getInvoice = async (
+    req: Request,
+    res: TypedResponse<Omit<InvoiceResponseDto, "fees"> & { fees: Record<string, any> }>,
+    next: NextFunction
+) => {
     try {
         const invoice = await Invoice.findById(req.params.id).populate<{ pharmacist: PharmacistDocument }>("pharmacist");
 
@@ -13,7 +19,30 @@ const getInvoice = async (req: Request, res: TypedResponse<InvoiceResponseDto>, 
             res.status(400).json({ success: false, details: [responseMessages.NOT_FOUND] });
             return;
         }
-        res.json({ success: true, data: toInvoiceResponseDto(invoice) });
+        let serializedDoc = toInvoiceResponseDto(invoice);
+        const sections = await Section.find().populate<{ fees: FeeDocument[] }>("fees");
+        let serializedFees: Record<string, any> = {};
+
+        for (const section of sections) {
+            let sectionTotalFeesValue = 0;
+            const sectionFees = section.fees.map((fee) => {
+                const currentFee = serializedDoc.fees?.find((element) => element.name == fee.name);
+                if (!currentFee) {
+                    return;
+                }
+                sectionTotalFeesValue += Number(currentFee.value);
+                return {
+                    name: fee.name,
+                    value: currentFee.value,
+                    numOfYears: currentFee.numOfYears,
+                };
+            });
+            serializedFees[section.name] = {};
+            serializedFees[section.name]["fees"] = sectionFees;
+            serializedFees[section.name]["total"] = sectionTotalFeesValue;
+        }
+
+        res.json({ success: true, data: { ...serializedDoc, fees: serializedFees } });
     } catch (e) {
         next(e);
     }
